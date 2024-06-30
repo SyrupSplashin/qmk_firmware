3dac55753e8be47e15d104154f99ab11a56dc3e1
SyrupSplashin <vertex@syrupsplash.in>
2024-06-30 01:12:28 -0400

    Added CapsWord-on-DoubleTap, created a few comments for organization's sake

--- keyboards/crkbd/keymaps/syrupsplashin/rules.mk
+++ keyboards/crkbd/keymaps/syrupsplashin/rules.mk
@@ -4,5 +4,6 @@ RGB_MATRIX_ENABLE = no    # Enable WS2812 RGB underlight.
 VIA_ENABLE      = no    # Enable VIA
 OLED_ENABLE     = yes
 LTO_ENABLE      = yes
+CAPS_WORD_ENABLE = yes
 #TAP_DANCE_ENABLE = yes
 # WPM_ENABLE = yes
