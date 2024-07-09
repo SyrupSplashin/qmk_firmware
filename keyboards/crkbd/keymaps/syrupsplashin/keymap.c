bf04ae154624d7133347b9822682b76455d1f1ef
SyrupSplashin <cbmgraphics@protonmail.ch>
2024-07-08 22:08:43 -0400

    Experimental "gaming" layer(s)

--- keyboards/crkbd/keymaps/syrupsplashin/keymap.c
+++ keyboards/crkbd/keymaps/syrupsplashin/keymap.c
@@ -47,41 +47,73 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //                        │     │     │     │   │     │ 0 │     │
 //                        └─────┴─────┴─────┘   └─────┴───┴─────┘
 [1] = LAYOUT_split_3x6_3(
-      KC_ESCAPE , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC ,     KC_PLUS  , KC_7 , KC_8    , KC_9 , KC_SLASH , KC_BSLS    ,
-      KC_QUES   , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN ,     KC_MINUS , KC_4 , KC_5    , KC_6 , KC_ASTR  , KC_BSPC    ,
-      KC_TRNS   , KC_TRNS , KC_LBRC , KC_RBRC , KC_LCBR , KC_RCBR ,     KC_DOT   , KC_1 , KC_2    , KC_3 , KC_EQUAL , KC_TRNS    ,
-                                      KC_TRNS , KC_TRNS , KC_TRNS ,     KC_TRNS  , KC_0 , KC_TRNS
+      KC_ESC  , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC ,     KC_PLUS  , KC_7 , KC_8    , KC_9 , KC_SLASH , KC_BSLS    ,
+      KC_QUES , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN ,     KC_MINUS , KC_4 , KC_5    , KC_6 , KC_ASTR  , KC_BSPC    ,
+      KC_TRNS , KC_TRNS , KC_LBRC , KC_RBRC , KC_LCBR , KC_RCBR ,     KC_DOT   , KC_1 , KC_2    , KC_3 , KC_EQUAL , KC_TRNS    ,
+                                    KC_TRNS , KC_TRNS , KC_TRNS ,     KC_TRNS  , KC_0 , KC_TRNS
 ),
 
-//        ┌────┬────┬──────┬──────┬──────┬─────┐   ┌──────┬──────┬──────┬──────┬─────┬─────┐
-//        │ no │ no │  no  │ volu │  no  │ no  │   │ pgup │ home │  up  │ end  │ no  │ no  │
-//        ├────┼────┼──────┼──────┼──────┼─────┤   ├──────┼──────┼──────┼──────┼─────┼─────┤
-//        │ no │ no │ mprv │ vold │ mnxt │ no  │   │ pgdn │ left │ down │ rght │ no  │ no  │
-//        ├────┼────┼──────┼──────┼──────┼─────┤   ├──────┼──────┼──────┼──────┼─────┼─────┤
-//        │ no │ no │  no  │ mute │  no  │ no  │   │ pscr │  no  │  no  │ del  │     │     │
-//        └────┴────┴──────┼──────┼──────┼─────┤   ├──────┼──────┼──────┼──────┴─────┴─────┘
-//                         │      │      │     │   │      │      │      │
-//                         └──────┴──────┴─────┘   └──────┴──────┴──────┘
+//        ┌─────┬────┬──────┬──────┬──────┬─────┐   ┌──────┬──────┬──────┬──────┬─────┬─────┐
+//        │ no  │ no │  no  │ volu │  no  │ no  │   │ pgup │ home │  up  │ end  │ no  │ no  │
+//        ├─────┼────┼──────┼──────┼──────┼─────┤   ├──────┼──────┼──────┼──────┼─────┼─────┤
+//        │     │ no │ mprv │ vold │ mnxt │ no  │   │ pgdn │ left │ down │ rght │ no  │ no  │
+//        ├─────┼────┼──────┼──────┼──────┼─────┤   ├──────┼──────┼──────┼──────┼─────┼─────┤
+//        │ no  │ no │  no  │ mute │  no  │ no  │   │ pscr │  no  │  no  │ del  │     │     │
+//        └─────┴────┴──────┼──────┼──────┼─────┤   ├──────┼──────┼──────┼──────┴─────┴─────┘
+//                          │      │      │     │   │      │      │      │
+//                          └──────┴──────┴─────┘   └──────┴──────┴──────┘
 [2] = LAYOUT_split_3x6_3(
       XXXXXXX , XXXXXXX , XXXXXXX , KC_VOLU , XXXXXXX , XXXXXXX ,     KC_PGUP , KC_HOME , KC_UP   , KC_END   , XXXXXXX , XXXXXXX    ,
-      XXXXXXX , XXXXXXX , KC_MPRV , KC_VOLD , KC_MNXT , XXXXXXX ,     KC_PGDN , KC_LEFT , KC_DOWN , KC_RIGHT , XXXXXXX , XXXXXXX    ,
+      KC_TRNS , XXXXXXX , KC_MPRV , KC_VOLD , KC_MNXT , XXXXXXX ,     KC_PGDN , KC_LEFT , KC_DOWN , KC_RIGHT , XXXXXXX , XXXXXXX    ,
       XXXXXXX , XXXXXXX , XXXXXXX , KC_MUTE , XXXXXXX , XXXXXXX ,     KC_PSCR , XXXXXXX , XXXXXXX , KC_DEL   , KC_TRNS , KC_TRNS    ,
                                     _______ , _______ , _______ ,     _______ , _______ , _______
 ),
 
-//        ┌────┬────┬────┬─────┬─────┬─────┐   ┌─────┬─────┬─────┬────┬─────┬─────┐
-//        │ no │ no │ no │ no  │ no  │ no  │   │ no  │ no  │ no  │ no │ no  │ no  │
-//        ├────┼────┼────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼────┼─────┼─────┤
-//        │ f1 │ f2 │ f3 │ f4  │ f5  │ f6  │   │ no  │ no  │ no  │ no │ no  │ no  │
-//        ├────┼────┼────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼────┼─────┼─────┤
-//        │ f7 │ f8 │ f9 │ f10 │ f11 │ f12 │   │ no  │ no  │ no  │ no │     │     │
-//        └────┴────┴────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼────┴─────┴─────┘
+//        ┌────┬────┬────┬─────┬─────┬─────┐   ┌─────┬─────┬─────┬────┬─────┬───────┐
+//        │ no │ no │ no │ no  │ no  │ no  │   │ no  │ no  │ no  │ no │ no  │ DF(4) │
+//        ├────┼────┼────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼────┼─────┼───────┤
+//        │ f1 │ f2 │ f3 │ f4  │ f5  │ f6  │   │ no  │ no  │ no  │ no │ no  │  no   │
+//        ├────┼────┼────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼────┼─────┼───────┤
+//        │ f7 │ f8 │ f9 │ f10 │ f11 │ f12 │   │ no  │ no  │ no  │ no │     │       │
+//        └────┴────┴────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼────┴─────┴───────┘
 //                       │     │     │     │   │     │     │     │
 //                       └─────┴─────┴─────┘   └─────┴─────┴─────┘
 [3] = LAYOUT_split_3x6_3(
-      XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX    ,
+      XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , DF(4)      ,
       KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   ,     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX    ,
       KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  ,     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_TRNS , KC_TRNS    ,
+                                    _______ , _______ , _______ ,     _______ , _______ , _______
+),
+
+//        ┌─────┬──────┬───┬─────┬───────┬─────┐   ┌─────┬─────┬─────┬────┬────┬───────┐
+//        │ esc │  no  │ q │  w  │   e   │  r  │   │ no  │ no  │ no  │ no │ no │ DF(0) │
+//        ├─────┼──────┼───┼─────┼───────┼─────┤   ├─────┼─────┼─────┼────┼────┼───────┤
+//        │ tab │ lsft │ a │  s  │   d   │  f  │   │ no  │ no  │ no  │ no │ no │  no   │
+//        ├─────┼──────┼───┼─────┼───────┼─────┤   ├─────┼─────┼─────┼────┼────┼───────┤
+//        │ no  │ lctl │ z │  x  │   c   │  v  │   │ no  │ no  │ no  │ no │ no │  no   │
+//        └─────┴──────┴───┼─────┼───────┼─────┤   ├─────┼─────┼─────┼────┴────┴───────┘
+//                         │     │ MO(5) │ spc │   │     │     │     │
+//                         └─────┴───────┴─────┘   └─────┴─────┴─────┘
+[4] = LAYOUT_split_3x6_3(
+      KC_ESC  , XXXXXXX , KC_Q , KC_W    , KC_E  , KC_R   ,     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , DF(0)      ,
+      KC_TAB  , KC_LSFT , KC_A , KC_S    , KC_D  , KC_F   ,     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX    ,
+      XXXXXXX , KC_LCTL , KC_Z , KC_X    , KC_C  , KC_V   ,     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX    ,
+                                 _______ , MO(5) , KC_SPC ,     _______ , _______ , _______
+),
+
+//        ┌────┬────┬────┬─────┬─────┬─────┐   ┌─────┬─────┬─────┬────┬────┬────┐
+//        │ no │ no │ no │ no  │ no  │ no  │   │ no  │ no  │ no  │ no │ no │ no │
+//        ├────┼────┼────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼────┼────┼────┤
+//        │ no │ 1  │ 2  │  3  │  4  │  5  │   │ no  │ no  │ no  │ no │ no │ no │
+//        ├────┼────┼────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼────┼────┼────┤
+//        │ no │ 6  │ 7  │  8  │  9  │  0  │   │ no  │ no  │ no  │ no │ no │ no │
+//        └────┴────┴────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼────┴────┴────┘
+//                       │     │     │     │   │     │     │     │
+//                       └─────┴─────┴─────┘   └─────┴─────┴─────┘
+[5] = LAYOUT_split_3x6_3(
+      XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX    ,
+      XXXXXXX , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX    ,
+      XXXXXXX , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    ,     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX    ,
                                     _______ , _______ , _______ ,     _______ , _______ , _______
 )
 };
